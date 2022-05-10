3175e5edeb54e8338f55da3e4146a3872d4c1035
Siu <siuc@protonmail.com>
2022-05-10 15:09:58 -0400

    Update 04_subsubcategories.sql

--- server/database/seeds/04_subsubcategories.sql
+++ server/database/seeds/04_subsubcategories.sql
@@ -1,7 +1,22 @@
 INSERT INTO subsubcategories (name, subcategory_id)
 VALUES
-  ('Women''s Fashion', 1),
+  ('Activewear', 1),
+  ('Maternity wear', 1),
+  ('Tops', 1),
+  ('Bottoms', 1),
+  ('Dresses & Sets', 1),
+  ('Footwear', 1),
+  ('Swimwear', 1),
+  ('Muslimah Fashion', 1),
+  ('Coats, Jackets and Outerwear', 1),
+  ('Bags & Wallets', 1),
+  ('Jewelry & Organisers', 1),
+  ('Watches & Accessories', 1),
+  ('New Undergarments & Loungewear', 1),
   ('Luxury', 1),
-  ('Men''s Fashion', 1),
-  ('Beauty & Personal Care', 1)
+  ('Bags & Wallets', 1),
+  ('Apparel', 1),
+  ('Accessories', 1),
+  ('Watches', 1),
+  ('Sneakers & Footwear', 1)
  ;
