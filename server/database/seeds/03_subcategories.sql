d057517947d4873b1c22b943e5d8f1258420b038
Siu <siuc@protonmail.com>
2022-05-13 12:35:16 -0400

    add subcatergories

--- server/database/seeds/03_subcategories.sql
+++ server/database/seeds/03_subcategories.sql
@@ -3,5 +3,9 @@ VALUES
   ('Women''s Fashion', 1),
   ('Luxury', 1),
   ('Men''s Fashion', 1),
-  ('Beauty & Personal Care', 1)
+  ('Beauty & Personal Care', 1),
+  ('Furniture & Home Living', 2),
+  ('Babies & Kids', 2),
+  ('Food & Drinks', 2),
+  ('Pet Supplies', 2)
  ;
