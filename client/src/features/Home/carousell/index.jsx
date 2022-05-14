5a447df474aaf8a259cfae8406bb087117a764ce
Siu <siuc@protonmail.com>
2022-05-13 20:36:18 -0400

    add alt to img

--- client/src/features/Home/carousell/index.jsx
+++ client/src/features/Home/carousell/index.jsx
@@ -7,11 +7,11 @@ import banner5 from "../banners/5.jpg";
 const Carousell = () => {
   return (
     <>
-      <img src={banner1} />
-      <img src={banner2} />
-      <img src={banner3} />
-      <img src={banner4} />
-      <img src={banner5} />
+      <img src={banner1} alt="" />
+      <img src={banner2} alt="" />
+      <img src={banner3} alt="" />
+      <img src={banner4} alt="" />
+      <img src={banner5} alt="" />
     </>
   );
 };
